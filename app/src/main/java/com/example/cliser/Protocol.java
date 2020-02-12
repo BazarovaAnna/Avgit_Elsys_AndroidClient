eb73b62d7c45da17117c715f9a3ffd5c44b1dfb0
PossessedKeepo <45289151+PossessedKeepo@users.noreply.github.com>
2020-02-12 15:11:58 +0300

    Protocol updated

--- app/src/main/java/com/example/cliser/Protocol.java
+++ app/src/main/java/com/example/cliser/Protocol.java
@@ -1,22 +1,43 @@
-package com.example.cliser;
+package com.example.elsysandroid;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import org.apache.commons.codec.binary.Hex;
 
-public class Protocol {
-    public static String GetDigest(String value, String key) {
+import java.io.ByteArrayOutputStream;
+import java.util.Random;
+
+public final class Protocol{
+    public static final String URL = "/xmlapi/std";
+
+    public static String GetNonce(){
+        byte[] nonce = new byte[20];
+        Random random = new Random();
+        random.nextBytes(nonce);
+        return nonce.toString();
+    }
+
+    public static String GetDigest(String aNonce, String aPassword, byte[] aContent, String aCreationTime){
         try {
             // Get an hmac_sha1 key from the raw key bytes
-            byte[] keyBytes = key.getBytes();
+            byte[] keyBytes = aPassword.getBytes();
             SecretKeySpec signingKey = new SecretKeySpec(keyBytes, "HmacSHA1");
 
             // Get an hmac_sha1 Mac instance and initialize with the signing key
             Mac mac = Mac.getInstance("HmacSHA1");
             mac.init(signingKey);
 
+
             // Compute the hmac on input data bytes
-            byte[] rawHmac = mac.doFinal(value.getBytes());
+            ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+            outStream.write(aNonce.getBytes());
+            outStream.write(aCreationTime.getBytes());
+            outStream.write("POST".getBytes());
+            outStream.write(Protocol.URL.getBytes());
+            outStream.write(aContent);
+
+            byte[] rawHmac = mac.doFinal(outStream.toByteArray());
+
 
             // Convert raw bytes to Hex
             byte[] hexBytes = new Hex().encode(rawHmac);
@@ -26,8 +47,5 @@ public class Protocol {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-    public class Protocol{
-
     }
 }
