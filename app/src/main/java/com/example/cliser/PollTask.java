4c993364fab7d3d801a17fba9dfb3db8bf439dce
root <root@Comp.localdomain>
2020-02-12 14:09:39 +0300

    added some functions, need java fixes

--- app/src/main/java/com/example/cliser/PollTask.java
+++ app/src/main/java/com/example/cliser/PollTask.java
@@ -1,24 +1,156 @@
 package com.example.cliser;
 
 public class PollTask {
-    String serverIP,password;
+    String ServerIP,Password;
+    boolean Connection;
+    int CID,SID;
+    int CommandID;
+    boolean Terminated;
+    private byte[] Content;
+    String RequestUri;
+
     public void Start(String aServerIP, String aPassword)
     {
-        this.serverIP = aServerIP;
-        this.password = aPassword;
+        this.ServerIP = aServerIP;
+        this.Password = aPassword;
 
         HTTPClient = new HttpClient { Timeout = TimeSpan.FromMilliseconds(15000) };
         HTTPResponse = null;
         CancelTokenSource = new CancellationTokenSource();
-        RequestUri = String.Format("http://{0}{1}", ServerIP, Protocol.URL);
+        RequestUri = String.format("http://{0}{1}", ServerIP, Protocol.URL);
 
         Connection = false;
         CID = 10000;
         SID = 0;
         CommandID = 10000;
         Terminated = false;
-        OnMessage?.Invoke("Начало опроса", "");
+        SocketClient.chText("Начало опроса");
         NextPoll();
 
+    }
+
+    private void NextPoll()
+    {
+        if (Terminated)
+        {
+            TerminatePoll();
+        }
+        else
+        {
+            PrepareRequest();
+            SendRequestAsync();
+        }
+    }
+
+    private void TerminatePoll()
+    {
+        if (HTTPClient != null)
+        {
+            HTTPClient.CancelPendingRequests();
+            HTTPClient.Dispose();
+        }
+    }
+
+    private void PrepareRequest()
+    {
+        String Nonce = Protocol.GetNonce();
+        DateTime now = DateTime.Now;
+        String CreationTime = (now.ToUniversalTime() + TimeCorrection).ToString("yyyy-MM-ddTHH:mm:ss.fffZ");
+
+        if (Math.abs(TimeCorrection.TotalSeconds) > 5)
+        {
+            OnMessage("Синхронизация времени", "");
+            XContent = Protocol.GetXContent(IncCID(), SID, now);
+        }
+        else
+        {
+            XContent = Protocol.GetXContent(IncCID(), SID);
+        }
+        Content = Encoding.UTF8.GetBytes(XContent.ToString());
+
+        String Digest = ElsysSDK2.GetDigest(Nonce, Password, Content, CreationTime);
+
+        HTTPClient.DefaultRequestHeaders.Clear();
+        HTTPClient.DefaultRequestHeaders.Add("ECNC-Auth", String.Format("Nonce=\"{0}\", Created=\"{1}\", Digest=\"{2}\"", Nonce, CreationTime, Digest));
+        HTTPClient.DefaultRequestHeaders.Date = now.ToUniversalTime();
+        HTTPClient.DefaultRequestHeaders.ConnectionClose = true;
+    }
+
+    private void SendRequestAsync()
+    {
+        try
+        {
+            if (XContent != null)
+            {
+                WriteToLog(new XElement("Client", new XAttribute("LocalTime", DateTime.Now.ToLocalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")), XContent));
+            }
+
+            HTTPResponse = await HTTPClient.PostAsync(RequestUri, new ByteArrayContent(Content), CancelTokenSource.Token);
+        }
+        catch(Exception e)
+        {
+            HTTPResponse = null;
+        }
+        HandleResponse();
+        NextPoll();
+
+    }
+
+    private void HandleResponse()
+    {
+        boolean connection = false;
+        if (!CancelTokenSource.IsCancellationRequested)
+            if (HTTPResponse != null)
+                if ((HTTPResponse.StatusCode == HttpStatusCode.OK) || (HTTPResponse.StatusCode == HttpStatusCode.Unauthorized))
+                {
+                    connection = true;
+                    if (HTTPResponse.Headers.Date.HasValue)
+                        TimeCorrection = HTTPResponse.Headers.Date.Value - DateTime.Now.ToUniversalTime();
+
+                    try
+                    {
+                        XDocument Content = XDocument.Parse(HTTPResponse.Content.ReadAsStringAsync().Result);
+                        if (Content.Root != null)
+                        {
+                            WriteToLog(new XElement("MBNet", new XAttribute("LocalTime", DateTime.Now.ToLocalTime().ToString("yyyy-MM-ddTHH:mm:ss.fffZ")), Content.Root));
+                            var BodyNodes = Content.Element("Envelope").Element("Body").Elements();
+                            foreach (var node in BodyNodes)
+                            {
+                                if (node.Name == "CIDResp") uint.TryParse(node.Value, out CIDResp);
+                                if (node.Name == "SID") uint.TryParse(node.Value, out SID);
+                                if (node.Name == "Events") HandleEvents(node);
+                                if (node.Name == "DevStates") HandleDevStates(node);
+                                if (node.Name == "OnlineStatus") HandleOnlineStatus(node);
+                                if (node.Name == "UpdSysConfigResponse") HandleInitDevTree(node);
+                                if (node.Name == "UpdAPBConfigResponse") HandleLoadAPB(node);
+                                if (node.Name == "ChangesResults") HandleChangesResult(node);
+                                if (node.Name == "ChangesResponse") HandleChangesResponse(node);
+                                if (node.Name == "ErrCode") HandleError(node.Value);
+                                if (node.Name == "ConfigGUID") CheckConfigGUID(node.Value);
+                                if (node.Name == "ConnectedDevices") HandleConnectedDevices(node);
+                                if (node.Name == "DisconnectedDevices") HandleDisconnectedDevices(node);
+                                if (node.Name == "ConnectedMBNets") HandleConnectedMBNets(node);
+                                if (node.Name == "DisconnectedMBNets") HandleDisconnectedMBNets(node);
+                                if (node.Name == "ControlCmdsResponse") HandleControlCmdsResponse(node);
+                                if (node.Name == "NumericalHWParams") HandleNumericalHWParams(node);
+                            }
+                            //totodo здесь нужно проверять наличие требуемого узла, чтобы завершать инициализацию при отсутствии ответов
+                            //CheckInit();
+                        }
+                    }
+                    catch(Exception e)
+                    {
+                    }
+                }
+
+        if (Connection != connection)
+        {
+            Connection = connection;
+
+            if (Connection)
+                SocketClient.chText("Восстановление связи");
+        else
+            SocketClient.chText("Потеря связи");
+        }
     }
 }
--- app/src/main/java/com/example/cliser/SocketClient.java
+++ app/src/main/java/com/example/cliser/SocketClient.java
@@ -13,110 +13,28 @@ import java.net.Socket;
 public class SocketClient {
 
     //реализовать клиентскую часть клиент-серверного приложения
-    private String mServerMessage;
-    private OnMessageReceived mMessageListener = null;
-    private boolean mRun = false; // флаг, определяющий, запущен ли сервер
-    private PrintWriter mBufferOut;
-    private BufferedReader mBufferIn;
-    private Socket socket;
-    private String passwd="12345678";
 
-    public SocketClient() {
-        /*TODO
-        Порядок действий
-        1) старт
-            передать нужный ip
-            передать нужный пароль (использовать нужный пароль)
-        2) клик
-            http запрос на клик
-         */
-    }
-
-    public void sendMessage(String message) {
-        if (mBufferOut != null && !mBufferOut.checkError()) {
-            String m=ElsysSDK2.GetDigest(message,passwd);
-            mBufferOut.println(m);
-            mBufferOut.flush();
-        }
-    }
-
-    public void stopClient() {
-        sendMessage("CLOSED_CONNECTION");//todo что отправляем при отсоединении?
-
-        mRun = false;
-
-        if (mBufferOut != null) {
-            mBufferOut.flush();
-            mBufferOut.close();
-        }
-
-        mMessageListener = null;
-        mBufferIn = null;
-        mBufferOut = null;
-        mServerMessage = null;
-    }
-
-    public void run() {
-        try {
-            //InetAddress serverAddr = InetAddress.getByName(address);todo? как это работает и что нам тут указывать
-
-            try {
-                socket = new Socket("serverAddr", 0000);//0000=SERVER PORT todo какой у нас порт сервера?
-                mRun = true;
-                mBufferOut =
-                        new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())),
-                                true);
-
-                mBufferIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                sendMessage("LOGIN_NAME"); // todo что мы должны отправлять в качестве приветствия во время тройного рукопожатия?
-                mMessageListener.onConnected();
-
-                // ждем ответа
-                while (mRun) {
-                    if (mBufferOut.checkError()) {
-                        mRun = false;
-                    }
-
-                    mServerMessage = mBufferIn.readLine();//todo расшифровать? это вообще нужно? а он вообще шиврует то что нам отправляет?
-
-                    if (mServerMessage != null && mMessageListener != null) {
-                        mMessageListener.messageReceived(mServerMessage);
-                    }
-                }
-                //todo не понимаю где вставить сам http обмен, по идее здесь где-то но где конкретно хз, и там уже реализовать формирование заголовков и т.п.
-            } catch (Exception e) {
-            } finally {
-                if (socket != null && socket.isConnected()) {
-                    socket.close();
-                }
-            }
-        } catch (Exception e) {
-        }
-    }
-
-    public boolean isConnected() {
-        return socket != null && socket.isConnected();
-    }
-
-    public boolean isRunning() {
-        return mRun;
-    }
-
-    public interface OnMessageReceived {
-        void messageReceived(String message);
-        void onConnected();
-    }
+    private boolean start=false;
+    private static TextView tv;
 
     public void buttonClicked(String btnName, TextView textV){
         if(btnName.equals("butt1")){
 
             textV.setText("Clicked");
+            tv=textV;
             //сюда поместить обработчик события - что именно мы хотим сделать по кнопке
             //sendMessage("");//todo   need a fix
             String textIP="192.168.1.21";
             String textPassword="12345678";
-            PollTask.Start(textIP, textPassword);
+            if(!start){
+                start=true;
+                PollTask pt=new PollTask();
+                pt.Start(textIP, textPassword);
+            }
 
         }
+    }
+    public static void chText(String str){
+        tv.setText(str);
     }
 }
